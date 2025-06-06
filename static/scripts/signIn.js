a234a0f86f093f99ca4990fb3ab4dc1ce391b77b
JSundB <johannes.sundback.2002@gmail.com>
2025-06-06 17:30:40 +0300

    Login stuff

---
+++ static/scripts/signIn.js
@@ -0,0 +1,71 @@
+//Auth link https://01.gritlab.ax/api/auth/signin
+
+export function SignInUsingCredentials(nameOrEmail, password) {
+    const credentials = btoa(`${nameOrEmail}:${password}`); //encode string in base-64
+
+    fetchToken(credentials)
+}
+
+
+
+/* async function logIn(e) {
+    e.preventDefault(); // don't reload at submit form
+    const nameOrEmail = e.target.useremail.value;
+    const password = e.target.password.value;
+    const credentials = btoa(`${nameOrEmail}:${password}`); //encode string in base-64
+
+    const [success, result] = await getJWT(credentials);
+
+    if (success) {
+        localStorage.setItem("jwt", result);  // Store JWT for future requests
+        localStorage.setItem("username", nameOrEmail); // Store username for display
+        loginErrorMessage.textContent = '';
+        console.log("Login successful");
+        updateUI();
+        loadContent();
+    } else {
+        console.error("Login failed:", result);
+        loginErrorMessage.textContent = result;
+    }
+} */
+
+
+export async function fetchToken(credentials) {
+    try {
+        const response = await fetch("https://01.gritlab.ax/api/auth/signin", {
+            method: "POST",
+            headers: {
+                "Authorization": `Basic ${credentials}`,
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+        if (response.ok) {
+            return [true, data];
+        } else {
+            return [false, data.error];
+        }
+    } catch (error) {
+        return [false, "Login not allowed from this location"];
+    }
+}
+
+export async function checkToken(token) {
+    try {
+        const response = await fetch("https://01.gritlab.ax/api/auth/signin", { // Change link
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+        if (response.ok) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return [false, "Login not allowed from this location"];
+    }
+}
